452f02ef27ce6f2baaa1caa4541b03871eee862c
Dale <dwebb@alacrityfoundation.com>
2013-12-30 23:00:41 +0000

    added working files

---
+++ public/javascripts/controllers.js
@@ -0,0 +1,90 @@
+var polls_count = 0;
+// Controller for the poll list
+function PollListCtrl($scope, Poll) {
+	$scope.polls = Poll.query();
+	polls_count = $scope.polls.length;
+}
+
+// Controller for an individual poll
+function PollItemCtrl($scope, $location, $routeParams, socket, Poll) {	
+	$scope.poll = Poll.get({pollId: $routeParams.pollId});
+	
+	socket.on('myvote', function(data) {
+		console.dir(data);
+		if(data._id == $routeParams.pollId) {
+			$scope.poll = data;
+		}
+	});
+	
+	socket.on('vote', function(data) {
+		console.dir(data);
+		if(data._id == $routeParams.pollId) {
+		  $scope.poll = data;
+		}		
+	});
+	
+	$scope.vote = function() {
+		var pollId = $scope.poll._id,
+				choiceId = $scope.poll.userVote;
+		
+		if(choiceId) {
+			var voteObj = { poll_id: pollId, choice: choiceId };
+			socket.emit('send:vote', voteObj);
+		} else {
+			alert('You must select an option to vote for');
+		}
+	};
+}
+
+// Controller for creating a new poll
+function PollNewCtrl($scope, $location, Poll) {
+	// Define an empty poll model object
+	$scope.poll = {
+	  _id: polls_count+1,
+		question: '',
+		choices: [ { _id: 1, text: '', votes: [] }, { _id: 2, text: '', votes: [] }, { _id: 3, text: '', votes: [] }]
+	};
+	
+	// Method to add an additional choice option
+	$scope.addChoice = function() {
+		$scope.poll.choices.push({ _id: $scope.poll.choices.length+1, text: '', votes: [] });
+	};
+	
+	// Validate and save the new poll to the database
+	$scope.createPoll = function() {
+		var poll = $scope.poll;
+		
+		// Check that a question was provided
+		if(poll.question.length > 0) {
+			var choiceCount = 0;
+			
+			// Loop through the choices, make sure at least two provided
+			for(var i = 0, ln = poll.choices.length; i < ln; i++) {
+				var choice = poll.choices[i];
+				
+				if(choice.text.length > 0) {
+					choiceCount++
+				}
+			}
+		
+			if(choiceCount > 1) {
+				// Create a new poll from the model
+				var newPoll = new Poll(poll);
+				
+				// Call API to save poll to the database
+				newPoll.$save(function(p, resp) {
+					if(!p.error) {
+						// If there is no error, redirect to the main view
+						$location.path('polls');
+					} else {
+						alert('Could not create poll');
+					}
+				});
+			} else {
+				alert('You must enter at least two choices');
+			}
+		} else {
+			alert('You must enter a question');
+		}
+	};
+}
---
+++ routes/index.js
@@ -0,0 +1,106 @@
+// Connect to MongoDB using Mongoose
+var taffy = require('taffydb').taffy;
+var db = taffy();
+
+// Main application view
+exports.index = function(req, res) {
+	res.render('index');
+};
+
+// JSON API for list of polls
+exports.list = function(req, res) {
+	// Query Mongo for polls, just get back the question text
+	res.json(db().get());
+};
+
+// JSON API for getting a single poll
+exports.poll = function(req, res) {
+	// Poll ID comes in the URL
+	var pollId = req.params.id;
+	// Find the poll by its ID, use lean as we won't be changing it
+	var poll = db({ _id: parseInt(pollId)}).first();
+	
+		if(poll) {
+			var userVoted = false,
+					userChoice,
+					totalVotes = 0;
+
+			// Loop through poll choices to determine if user has voted
+			// on this poll, and if so, what they selected
+			for(c in poll.choices) {
+				var choice = poll.choices[c]; 
+
+				for(v in choice.votes) {
+					var vote = choice.votes[v];
+					totalVotes++;
+
+					if(vote.ip === (req.header('x-forwarded-for') || req.ip)) {
+						userVoted = true;
+						userChoice = { _id: choice._id, text: choice.text };
+					}
+				}
+			}
+
+			// Attach info about user's past voting on this poll
+			poll.userVoted = userVoted;
+			poll.userChoice = userChoice;
+
+			poll.totalVotes = totalVotes;
+		
+			res.json(poll);
+		} else {
+			res.json({error:true});
+		}
+};
+
+// JSON API for creating a new poll
+exports.create = function(req, res) {
+	var reqBody = req.body,
+			// Filter out choices with empty text
+			choices = reqBody.choices.filter(function(v) { return v.text != ''; }),
+			// Build up poll object to save
+			pollObj = {_id: db().get().length+1, question: reqBody.question, choices: choices};
+				
+	// Create poll model from built up poll object
+	doc = db.insert(pollObj);
+	
+	res.json(doc);
+};
+
+exports.vote = function(socket) {
+	socket.on('send:vote', function(data) {
+		var ip = socket.handshake.headers['x-forwarded-for'] || socket.handshake.address.address;
+		
+		var poll = db({ _id: parseInt(data.poll_id)}).first();
+		
+			var choice = poll.choices[data.choice-1];
+			choice.votes.push({ ip: ip });
+			db(poll).remove();
+			db.merge(poll);
+			var doc = poll;
+				var theDoc = { 
+					question: doc.question, _id: doc._id, choices: doc.choices, 
+					userVoted: false, totalVotes: 0 
+				};
+
+				// Loop through poll choices to determine if user has voted
+				// on this poll, and if so, what they selected
+				for(var i = 0, ln = doc.choices.length; i < ln; i++) {
+					var choice = doc.choices[i]; 
+
+					for(var j = 0, jLn = choice.votes.length; j < jLn; j++) {
+						var vote = choice.votes[j];
+						theDoc.totalVotes++;
+						theDoc.ip = ip;
+
+						if(vote.ip === ip) {
+							theDoc.userVoted = true;
+							theDoc.userChoice = { _id: choice._id, text: choice.text };
+						}
+					}
+				}
+				
+				socket.emit('myvote', theDoc);
+				socket.broadcast.emit('vote', theDoc);
+			});			
+};
